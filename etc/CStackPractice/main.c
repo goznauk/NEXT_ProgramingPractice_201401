f7b8c2b7061b98813a00dc6d7fa45e330b283ec9
goznauk <goznauk@nhnnext.org>
2014-04-08 20:16:00 +0900

    Error Function

--- etc/CStackPractice/main.c
+++ etc/CStackPractice/main.c
@@ -4,13 +4,20 @@
 #include <stdbool.h>
 
 
-
 const int STACKSIZE = 100;
 const int INPUTSIZE = 200;
 const int OPENER = 2;
 const int CLOSER = 3;
+const int ERROR_STACK_OVERFLOW = -1;
+const int ERROR_STACK_UNDERFLOW = -2;
+const int ERROR_UNEXPECTED_INPUT = -3;
+const int ERROR_UNCORRECT_INPUT = -4;
+const int ERROR_UNEXPECTED_END = -5;
 const char LINE[]  = "|--------|-------|------|";
 const char LINEN[] = "|--------|-------|------|\n";
+
+int i=0;
+char mStr[INPUTSIZE];
 
 typedef struct {
   int id;
@@ -65,13 +72,45 @@ STACK* initStack() {
   return ps;
 }
 
+ITEM getTop(STACK *ps)     { return ps->items[(ps->size)-1]; }
+char getTopChar(STACK *ps) { return getTop(ps).ch; }
+
 bool isStackEmpty(STACK *ps) { return ps->size == 0; }
 bool isStackFull (STACK *ps) { return ps->size == STACKSIZE; }
 
+void endWithError(STACK *ps, int errorCode) {
+  switch(errorCode) {
+	case ERROR_STACK_OVERFLOW :
+    fprintf(stderr, "%s\tError : Stack Overflow\n", LINE);
+    break;
+  
+	// May never be happened lol
+	case ERROR_STACK_UNDERFLOW :
+    fprintf(stderr, "%s\tError : Stack Underflow\n", LINE);
+		break;
+
+	case ERROR_UNEXPECTED_INPUT :
+      fprintf(stderr, "%s\tError : Unexpected Input - (at %d) cahracter %c\n%s\tPlease Input only (){}[]\n", LINE, i+1, mStr[i], LINE);
+		break;
+
+	case ERROR_UNCORRECT_INPUT :
+        fprintf(stderr, "%s\tError : Uncorrect Input\n%s\t(at %d) character %c should be replaced\n", LINE, LINE, i+1, mStr[i]);
+		break;
+			
+	case ERROR_UNEXPECTED_END :
+    fprintf(stderr, "%s\tError : Unexpected End\n%s\t%c has to be closed (started at %d)\n", LINE, LINE, getTopChar(ps), getTop(ps).id);
+		break;
+	}
+
+  printf("└-----------------------┘\n");
+  free(ps);
+}
+
 void push(STACK *ps, ITEM x) {
   if(isStackFull(ps)) {
-    fprintf(stderr, "%s\tError : Stack Overflow\n", LINE);
-    return;
+		endWithError(ps, ERROR_STACK_OVERFLOW);
+		exit(isStackFull(ps));
+    //return;
   }
   ps->items[ps->size] = x;
   printf("|  push\t |  %03d  |  %c%c  |\n", ps->items[ps->size].id, ps->items[ps->size].ch, getPair(ps->items[ps->size].ch));
@@ -80,22 +119,19 @@ void push(STACK *ps, ITEM x) {
 
 ITEM pop(STACK *ps) {
   if(isStackEmpty(ps)) {
-    fprintf(stderr, "%s\tError : Stack Underflow\n", LINE);
-    ITEM emptyItem;
-    return emptyItem;
+		endWithError(ps, ERROR_STACK_UNDERFLOW);
+		exit(isStackEmpty(ps));
+    //ITEM emptyItem;
+    //return emptyItem;
   }
   ps->size--;
   printf("|  pop\t |  %03d  |  %c%c  |\n", ps->items[ps->size].id, ps->items[ps->size].ch, getPair(ps->items[ps->size].ch));
   return ps->items[ps->size];
 }
 
-ITEM getTop(STACK *ps)     { return ps->items[(ps->size)-1]; }
-char getTopChar(STACK *ps) { return getTop(ps).ch; }
 
 int main() {
   STACK *ps = initStack();
-
-  char mStr[INPUTSIZE];
 
   printf("Input : ");
   fgets(mStr, INPUTSIZE, stdin);
@@ -107,9 +143,9 @@ int main() {
   printf("|        | point |      |\n");
   printf("|--------|-------|------|\n");
 
-  for(int i=0; i<strlen(mStr); i++) {
+  for(i=0; i<strlen(mStr); i++) {
     if(!getType(mStr[i])) {
-      fprintf(stderr, "%s\tError : Unexpected Input - (at %d) cahracter %c\n%s\tPlease Input only (){}[]\n", LINE, i+1, mStr[i], LINE);
+			endWithError(ps, ERROR_UNEXPECTED_INPUT);
       return 1;
     }
     else if(getType(mStr[i]) == OPENER) {
@@ -123,14 +159,14 @@ int main() {
         pop(ps);
       }
       else {
-        fprintf(stderr, "%s\tError : Uncorrect Input\n%s\t(at %d) character %c should be replaced\n", LINE, LINE, i+1, mStr[i]);
+				endWithError(ps, ERROR_UNCORRECT_INPUT);
         return 1;
       }
     }
   }
 
   if(ps->size!=0) {
-    fprintf(stderr, "%s\tError : Unexpected End\n%s\t%c has to be closed (started at %d)\n", LINE, LINE, getTopChar(ps), getTop(ps).id);
+		endWithError(ps, ERROR_UNEXPECTED_END);
     return 1;
   }
 
