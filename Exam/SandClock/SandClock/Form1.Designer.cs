dc47d5028121ce652af0f0f73d1632d6c68fd902
goznauk <goznauk@nhnnext.org>
2014-06-21 00:37:27 +0900

    Term End Exam

---
+++ Exam/SandClock/SandClock/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace SandClock {
+  partial class Form1 {
+    /// <summary>
+    /// 필수 디자이너 변수입니다.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 사용 중인 모든 리소스를 정리합니다.
+    /// </summary>
+    /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form 디자이너에서 생성한 코드
+
+    /// <summary>
+    /// 디자이너 지원에 필요한 메서드입니다.
+    /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.label1 = new System.Windows.Forms.Label();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(42, 13);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(0, 18);
+      this.label1.TabIndex = 0;
+      // 
+      // timer1
+      // 
+      this.timer1.Enabled = true;
+      this.timer1.Interval = 40;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // Form1
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+      this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+      this.BackColor = System.Drawing.Color.Linen;
+      this.ClientSize = new System.Drawing.Size(590, 567);
+      this.Controls.Add(this.label1);
+      this.DoubleBuffered = true;
+      this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Analog Clock";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Timer timer1;
+  }
+}
+
---
+++ Exam/SandClock/SandClock/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SandClock {
+
+  public enum Milliseconds { HALFDAY = 43200000, HOUR = 3600000, MINUTE = 60000, SECOND = 1000 };
+  public enum CenterCoordinate { X = 300, Y = 300, RADIOUS = 200, OUTERRADIOUS = 205 };
+
+  public partial class Form1 : Form {
+    Graphics mGraphics;
+    Coordinate center;
+    ClockCoordinate mClock;
+    Pen hourPen, minutePen, secondPen, mPen;
+
+    Bitmap B;
+
+    public Form1() {
+      InitializeComponent();
+
+      this.Paint += new PaintEventHandler(Form1_Paint);
+      center = new Coordinate((int)CenterCoordinate.X, (int)CenterCoordinate.Y);
+      mClock = new ClockCoordinate(center);
+      hourPen = new Pen(Color.Black, 5);
+      hourPen.Alignment = PenAlignment.Outset;
+      hourPen.EndCap = LineCap.DiamondAnchor;
+      minutePen = new Pen(Color.Black, 3);
+      secondPen = new Pen(Color.Red, 2);
+      secondPen.EndCap = LineCap.RoundAnchor;
+      mPen = new Pen(Color.Black);
+      mGraphics = this.CreateGraphics();
+      mGraphics.Clear(Color.Linen);
+
+      this.SetStyle(ControlStyles.DoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+    }
+
+    private void Form1_Paint(object sender, PaintEventArgs e) {
+      if (B != null) {
+        e.Graphics.DrawImage(B, 0, 0);
+      }
+    }          
+  
+
+    protected override void OnPaintBackground(PaintEventArgs e) {
+      //do not delete background
+      //base.OnPaintBackground(e);
+    }
+
+
+    private void timer1_Tick(object sender, EventArgs e) {
+      //manual double buffering - create bitmap
+      if (B == null || B.Width != ClientSize.Width || B.Height != ClientSize.Height) {
+        B = new Bitmap(ClientSize.Width, ClientSize.Height);
+      }
+      Graphics mGraphics = Graphics.FromImage(B);
+      mGraphics.Clear(SystemColors.Window);
+
+      //draw Clock
+      this.Text = "Analog Clock : " + DateTime.Now.ToString();
+
+      mClock.calculate(DateTime.Now);
+      mGraphics.SmoothingMode = SmoothingMode.HighQuality;
+
+      //clock
+      mGraphics.DrawEllipse(minutePen, 100, 100, 400, 400);
+
+      for (int i = 0; i < 60; i++) {
+        if (i % 5 == 0) {
+          // Draw 5 minute ticks
+          mGraphics.DrawLine(minutePen,
+          center.x + (float)((float)CenterCoordinate.RADIOUS / 1.00F * Math.Sin(i * 6 * Math.PI / 180)),
+          center.y - (float)((float)CenterCoordinate.RADIOUS / 1.00F * Math.Cos(i * 6 * Math.PI / 180)),
+          center.x + (float)((float)CenterCoordinate.RADIOUS / 1.12F * Math.Sin(i * 6 * Math.PI / 180)),
+          center.y - (float)((float)CenterCoordinate.RADIOUS / 1.12F * Math.Cos(i * 6 * Math.PI / 180)));
+        } else {
+          // draw 1 minute ticks
+          mGraphics.DrawLine(mPen,
+          center.x + (float)((float)CenterCoordinate.RADIOUS / 1.00F * Math.Sin(i * 6 * Math.PI / 180)),
+          center.y - (float)((float)CenterCoordinate.RADIOUS / 1.00F * Math.Cos(i * 6 * Math.PI / 180)),
+          center.x + (float)((float)CenterCoordinate.RADIOUS / 1.07F * Math.Sin(i * 6 * Math.PI / 180)),
+          center.y - (float)((float)CenterCoordinate.RADIOUS / 1.07F * Math.Cos(i * 6 * Math.PI / 180)));
+        }
+      }
+  
+      //hour
+      mGraphics.DrawLine(hourPen,
+        center.x, center.y, mClock.hCoordinate.x, mClock.hCoordinate.y);
+        mGraphics.DrawEllipse(hourPen, 294, 294, 10, 10);
+
+      //minute
+      mGraphics.DrawLine(minutePen,
+        center.x, center.y, mClock.mCoordinate.x, mClock.mCoordinate.y);
+      mGraphics.DrawEllipse(minutePen, 295, 295, 8, 8);
+
+      //second
+      mGraphics.DrawLine(secondPen,
+        center.x, center.y, mClock.sCoordinate.x, mClock.sCoordinate.y);
+
+      mGraphics.DrawEllipse(secondPen, 296, 296, 6, 6);
+
+      Invalidate();
+    }
+  }
+
+
+  public class Coordinate {
+    public float x, y;
+
+    public Coordinate(float x, float y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+
+  public class RadiousCoordinate {
+    Coordinate center;
+    double radious;
+    double degree; //radian
+
+    public RadiousCoordinate(Coordinate center, double degree) {
+      this.center = center;
+      this.degree = degree;
+    }
+
+    //size 0~1
+    public Coordinate toCoordinate(float size) {
+      radious = 0.98 * size * (float)CenterCoordinate.RADIOUS;
+      return new Coordinate(center.x + (float)(radious * Math.Sin(degree)),
+        center.y - (float)(radious * Math.Cos(degree)));
+    }
+  }
+
+  public class ClockCoordinate {
+    public bool isInitial = true;  
+    public Coordinate hCoordinate;
+    public Coordinate mCoordinate;
+    public Coordinate sCoordinate;
+
+    DateTime mDateTime;
+    int simpleTime;
+    Coordinate center;
+
+    public ClockCoordinate(Coordinate center) {
+      this.center = center;
+    }
+
+    public void calculate(DateTime time) {
+      isInitial = false;
+      RadiousCoordinate temp;
+      mDateTime = time;
+      simpleTime = (int)mDateTime.TimeOfDay.TotalMilliseconds;
+
+      //hour
+      simpleTime = simpleTime % (int)Milliseconds.HALFDAY;
+      temp = new RadiousCoordinate(center,
+        ((double)simpleTime / (int)Milliseconds.HALFDAY) * 2 * Math.PI);
+      hCoordinate = temp.toCoordinate(0.75F);
+
+      //minute
+      simpleTime = simpleTime % (int)Milliseconds.HOUR;
+      temp = new RadiousCoordinate(center,
+        ((double)simpleTime / (int)Milliseconds.HOUR) * 2 * Math.PI);
+      mCoordinate = temp.toCoordinate(0.9F);
+
+      //second
+      simpleTime = simpleTime % (int)Milliseconds.MINUTE;
+      simpleTime = simpleTime - simpleTime % 40;
+
+      temp = new RadiousCoordinate(center,
+        ((double)simpleTime / (int)Milliseconds.MINUTE) * 2 * Math.PI);
+      sCoordinate = temp.toCoordinate(1);
+
+      return;
+    }
+  }
+}
